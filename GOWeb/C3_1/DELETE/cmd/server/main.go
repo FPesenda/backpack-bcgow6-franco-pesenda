9e1aaae38bd43263a19188502d8481d3dc6754f4
FPesenda <franco.pesenda@mercadolibre.com>
2022-10-05 15:29:46 -0300

    finish C3_1 exersice 2

---
+++ GOWeb/C3_1/DELETE/cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/FPesenda/backpack-bcgow6-franco-pesenda/GOWeb/C3_1/DELETE/cmd/server/handler"
+	"github.com/FPesenda/backpack-bcgow6-franco-pesenda/GOWeb/C3_1/DELETE/internal/products"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//CRUD de PRODUCTOS
+	repository := products.NewRepository()
+	service := products.NewService(repository)
+	products := handler.NewProduct(service)
+	//CREACIÓN DEL ROUTER PRINCIPAL
+	router := gin.Default()
+	//CREAMOS EN EL RP UN GRUPO DE CONSULTAS
+	productsRouter := router.Group("/products")
+	{
+		productsRouter.DELETE("/:id", products.Delete())
+		productsRouter.GET("/", products.GetAll())
+		productsRouter.POST("/", products.Store())
+		productsRouter.PUT("/:id", products.UpdateByID())
+	}
+	//ARRANCAMOS EL ROUTER
+	router.Run()
+}
---
+++ GOWeb/C3_1/PATCH/internal/products/service.go
@@ -0,0 +1,53 @@
+package products
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	Delete(int) error
+	GetAll() ([]Products, error)
+	Store(string, string, string, float64) (Products, error)
+	UpdateByID(id int, name, color, code string, price float64) (product Products, err error)
+}
+
+func NewService(rep Repository) Service {
+	return &service{
+		repository: rep,
+	}
+}
+
+func (serv *service) Delete(id int) (err error) {
+	_, err = serv.repository.Delete(id)
+	return
+}
+
+func (s *service) GetAll() ([]Products, error) {
+	ps, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return ps, nil
+}
+
+func (serv *service) Store(name, color, code string, price float64) (Products, error) {
+	lastID, err := serv.repository.GetLastId()
+	if err != nil {
+		return Products{}, err
+	}
+
+	lastID++
+
+	producto, err := serv.repository.Store(lastID, name, color, code, price)
+	if err != nil {
+		return Products{}, err
+	}
+
+	return producto, nil
+}
+
+func (serv *service) UpdateByID(id int, name, color, code string, price float64) (product Products, err error) {
+	product, err = serv.repository.UpdateByID(id, name, color, code, price)
+	return
+}
